6a5433325e6df4b138a1cc45a5e0a8f3f638a3dd
amirhosien <amirhosein.madanyy@gmail.com>
2021-09-30 19:33:25 +0330

    add footer to the project for putting github link

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,19 @@
 
     </main>
 
+    <Footer />
+
 </template>
 
 <script>
 // components
 import Calculator from '@/components/Calculator'
+import Footer from '@/components/Footer'
 
 export default {
     components: {
-        Calculator
+        Calculator,
+        Footer
     }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+
+    <footer class="footer">
+
+        <a href="https://github.com/Amirmadany/calculator" class="footer-link">
+    
+            <span class="footer-link-content">GitHub</span>
+
+            <div class="footer-link-logo">
+                <img src="@/assets/github.svg" alt="">
+            </div>
+
+        </a>
+
+    </footer>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+    .footer{
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        background: white;
+        box-shadow: 0 0 15px 5px rgba(0, 0, 0, 0.13);
+        width: 100%;
+        text-align: center;
+        padding: 0.5rem 0;
+        font-size: 1rem;
+    }
+
+    .footer-link{
+        text-decoration: none;
+        color: #000;
+        display: flex;
+        align-content: center;
+        justify-content: center;
+        font-weight: bold;
+    }
+
+    .footer-link-content{
+        transform: translateY(0.34rem);
+    }
+
+</style>
